64f7bbd1d2c8c61d8c7e7dd66c9468ae320afbd0
yigittanyel <yigittanyel@trex.com.tr>
2023-03-30 16:31:46 +0300

    initial commit

---
+++ RabbitMQ.consumer/Program.cs
@@ -0,0 +1,28 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://hrpplbdi:EDX3pkhmXkX0xUZq4HuGcOA5ASB5Ykpu@whale.rmq.cloudamqp.com/hrpplbdi");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello", true, false, false);
+
+channel.BasicQos(0, 1, false);
+
+var consumer = new EventingBasicConsumer(channel);
+channel.BasicConsume("hello", false, consumer);
+
+consumer.Received += (object sender, BasicDeliverEventArgs e) =>
+{
+    var message = Encoding.UTF8.GetString(e.Body.ToArray());
+
+    Thread.Sleep(1000);
+    Console.WriteLine("Message is: " + message);
+
+    channel.BasicAck(e.DeliveryTag, false);
+};
+
+Console.ReadLine();
---
+++ RabbitMQ.producer/Program.cs
@@ -0,0 +1,21 @@
+﻿using RabbitMQ.Client;
+using RabbitMQ.Client.Events;
+using System.Linq;
+using System.Text;
+
+var factory = new ConnectionFactory();
+factory.Uri = new Uri("amqps://hrpplbdi:EDX3pkhmXkX0xUZq4HuGcOA5ASB5Ykpu@whale.rmq.cloudamqp.com/hrpplbdi");
+
+using var connection = factory.CreateConnection();
+var channel = connection.CreateModel();
+
+channel.QueueDeclare("hello", true, false, false);
+Enumerable.Range(1, 50).ToList().ForEach(x =>
+{
+    string message = $"Message {x}";
+    var messageBody = Encoding.UTF8.GetBytes(message);
+    channel.BasicPublish(string.Empty, "hello", null, messageBody);
+    Console.WriteLine($"Message send: {message}");
+});
+
+Console.ReadLine();
